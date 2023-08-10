1ec3bb4b46d16ed6fe09d85c1f2a111b6ea53ec0
Lelo <heloisacmk@hotmail.com>
2023-08-10 16:37:33 -0700

    Layout adjustments to fit and spread content better

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,34 @@
 import SelectMenu from './components/select-day-menu'
+import MiniSectionCards from './components/mini-section-cards'
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-12 h-full">
+    <div className="grid grid-cols-12 grid-rows-1 h-full">
       <div className="col-span-2">gap</div>
+ {/* --------------------------------------------MAIN-------------------------------------------------- */}
+      <div className="grid grid-rows-12 grid-cols-1 col-span-8">
 
-      <div className="grid grid-rows-4 grid-cols-1 col-span-8">
-
-        <div className="bg-pink flex justify-center">
+        <div className="bg-pink row-span-5 flex justify-center">
           <div className="bg-green w-2/4 flex flex-col justify-center">
             <h1></h1>
           </div>
         </div>
 
-        <div className="bg-blue flex justify-center">
+        <div className="bg-blue row-span-1 flex justify-center">
           <div className="bg-primary w-2/4 flex flex-col justify-center text-center">
             < SelectMenu />
           </div>
         </div>
 
-        <div className="bg-purple">other happy hours</div>
-        <div className="bg-yellow">maybe a map</div>
-      </div>
+        <div className="bg-purple row-span-3">
+          <div>
+            <MiniSectionCards />
+          </div>
+        </div>
 
+        <div className="bg-yellow row-span-3">maybe a map</div>
+      </div>
+{/* ------------------------------------------------------------------------------------------------ */}
       <div className="col-span-2">gap</div>
     </div>
   )
