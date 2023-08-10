aeb42df3c14ffd53f9f480320a77a88e67b7b223
Lelo <heloisacmk@hotmail.com>
2023-08-10 13:25:44 -0700

    right size for gap using grid system (col-span-1)

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 h-full">
-      <div className="">gap</div>
+    <div className="grid grid-cols-12 h-full">
+      <div className="col-span-1">gap</div>
 
-      <div className="grid grid-rows-4">
+      <div className="grid grid-rows-4 col-span-10">
         <div className="bg-blue">highlight</div>
         <div className="bg-pink">search</div>
         <div className="bg-purple">other happy hours</div>
         <div className="bg-yellow">maybe a map</div>
       </div>
 
-      <div className="">gap</div>
+      <div className="col-span-1">gap</div>
     </div>
   )
 }
