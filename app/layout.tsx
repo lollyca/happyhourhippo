2cc5ff01a5ad858ae73eec0ed077b4753bb3a2b0
Lelo <heloisacmk@hotmail.com>
2023-08-10 12:53:45 -0700

    basic grid layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="bg-strong-orange">{children}</body>
+    <html className="h-full" lang="en">
+      <body className="h-full">{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello My Friends</h1>
+    <div className="grid grid-rows-4 grid-cols-1 h-full">
+
+        <div className="bg-blue">highlight</div>
+        <div className="bg-pink">search</div>
+        <div className="bg-purple">other happy hours</div>
+        <div className="bg-yellow">maybe a map</div>
+
+
     </div>
   )
 }
