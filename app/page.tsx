5953182b6852ee54d726971095dd8e6689fe135f
Lelo <heloisacmk@hotmail.com>
2023-08-10 13:24:29 -0700

    page has 2 gaps and 1 main area for content

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-4 grid-cols-1 h-full">
+    <div className="grid grid-cols-3 h-full">
+      <div className="">gap</div>
 
+      <div className="grid grid-rows-4">
         <div className="bg-blue">highlight</div>
         <div className="bg-pink">search</div>
         <div className="bg-purple">other happy hours</div>
         <div className="bg-yellow">maybe a map</div>
+      </div>
 
-
+      <div className="">gap</div>
     </div>
   )
 }
