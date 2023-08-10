3bae22ae634d21e8ecde0d9a4c49843cb76cb4ba
Lelo <heloisacmk@hotmail.com>
2023-08-10 11:53:00 -0700

    added theme tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,45 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
     },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+
+      'strong-orange': '#ff6c44',
+      'light-orange': '#fff0ec',
+      'dark': '#1e1e1e',
+      'less-dark': '#595959',
+      'kinda-white': '#f3f3f3',
+
+      'primary': '#317caf',
+      'danger': '#a02f40',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
