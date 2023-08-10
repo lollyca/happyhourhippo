1ecf24078896a73448c7850877f7f73edded4f92
Lelo <heloisacmk@hotmail.com>
2023-08-10 14:49:41 -0700

    Added Select Menu Component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
+import SelectMenu from './components/select-day-menu'
 
 export default function Home() {
   return (
     <div className="grid grid-cols-12 h-full">
-      <div className="col-span-1">gap</div>
+      <div className="col-span-2">gap</div>
 
-      <div className="grid grid-rows-4 col-span-10">
-        <div className="bg-blue">highlight</div>
+      <div className="grid grid-rows-4 grid-cols-1 col-span-8">
+        <div className="bg-blue flex justify-center">
+          <div className="w-2/4">
+            < SelectMenu />
+          </div>
+        </div>
         <div className="bg-pink">search</div>
         <div className="bg-purple">other happy hours</div>
         <div className="bg-yellow">maybe a map</div>
       </div>
 
-      <div className="col-span-1">gap</div>
+      <div className="col-span-2">gap</div>
     </div>
   )
 }
