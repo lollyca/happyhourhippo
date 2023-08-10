451c513d17e8d22336ecd3c269e24e3b3c60dbfc
Lelo <heloisacmk@hotmail.com>
2023-08-10 16:23:25 -0700

    Layout with 2 half size divs and 2 full

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,19 @@ export default function Home() {
       <div className="col-span-2">gap</div>
 
       <div className="grid grid-rows-4 grid-cols-1 col-span-8">
+
+        <div className="bg-pink flex justify-center">
+          <div className="bg-green w-2/4 flex flex-col justify-center">
+            <h1></h1>
+          </div>
+        </div>
+
         <div className="bg-blue flex justify-center">
-          <div className="w-2/4">
+          <div className="bg-primary w-2/4 flex flex-col justify-center text-center">
             < SelectMenu />
           </div>
         </div>
-        <div className="bg-pink">search</div>
+
         <div className="bg-purple">other happy hours</div>
         <div className="bg-yellow">maybe a map</div>
       </div>
