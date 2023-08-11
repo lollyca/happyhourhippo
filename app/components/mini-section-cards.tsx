b82b3df57346f5d4b65bdc064c5e1ca941fd706b
Lelo <heloisacmk@hotmail.com>
2023-08-11 11:01:26 -0700

    addded picture do the mini card and impletement grid system to spread the title and cards better

--- app/components/mini-section-cards.tsx
+++ app/components/mini-section-cards.tsx
@@ -1,10 +1,10 @@
-const happyHourInfo = [
+const posts = [
   {
     id: 1,
     title: 'Boost your conversion rate',
     href: '#',
     description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta',
     date: 'Mar 16, 2020',
     datetime: '2020-03-16',
     category: { title: 'Bar', href: '#' },
@@ -58,67 +58,60 @@ const happyHourInfo = [
 
 export default function MiniSectionCards() {
   return (
-    <div className="bg-purple sm:py-32 h-full">
-      <div className="">
-
-        {/* --------------------------------------------------- Title --------------------------------------------------- */}
-        <div className="mx-auto max-w-2xl lg:mx-0">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Happy Hours:</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Check Happy Hours Available For Today!
-          </p>
-        </div>
-        {/* ------------------------------------------------------------------------------------------------------------- */}
+    <div className="grid grid-rows-5 grid-cols-1 grid-flow-col gap-4">
 
 
-
-        <div className="border-t border-gray-200 pt-6 mt-3">
-        {/* --------------------------------------------------- Corpo --------------------------------------------------- */}
-          <div className="bg-kinda-white flex justify-between">
-            {happyHourInfo.map((post) => (
-              <div key={post.id} className="flex max-w-xl flex-col items-start justify-between">
-                <div className="flex items-center gap-x-4 text-xs">
-                  {/* <time dateTime={post.datetime} className="text-gray-500">
-                    {post.date}
-                  </time> */}
-                  <p className="text-gray-500">M-T</p>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                  >
-                    {post.category.title}
-                  </a>
-                </div>
-                <div className="group relative">
-                  <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
-                </div>
-                {/* <div className="relative mt-8 flex items-center gap-x-4">
-                <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
-                <div className="text-sm leading-6">
-                  <p className="font-semibold text-gray-900">
-                    <a href={post.author.href}>
-                      <span className="absolute inset-0" />
-                      {post.author.name}
-                    </a>
-                  </p>
-                  <p className="text-gray-600">{post.author.role}</p>
-                </div>
-              </div> */}
-              </div>
-            ))}
-          </div>
-        {/* ------------------------------------------------------------------------------------------------------------- */}
-        </div>
-
-
-
+      <div className="bg-pink row-span-1 border-b border-gray-200 pt-5">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Learn how to grow your business with our expert advice.
+        </p>
       </div>
+
+
+      <div className="grid grid-cols-3 gap-x-8  bg-green row-span-4">
+        {posts.map((post) => (
+          <article key={post.id} className="flex max-w-xl flex-col items-start justify-between">
+            <div>
+              <img src={post.author.imageUrl} alt="" className="h-100 w-100 rounded" />
+            </div>
+            <div className="flex items-center gap-x-4 text-xs">
+              <time dateTime={post.datetime} className="text-gray-500">
+                {post.date}
+              </time>
+              <a
+                href={post.category.href}
+                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
+              >
+                {post.category.title}
+              </a>
+            </div>
+            <div className="group relative">
+              <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                <a href={post.href}>
+                  <span className="absolute inset-0" />
+                  {post.title}
+                </a>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{post.description}</p>
+            </div>
+            {/* <div className="relative mt-8 flex items-center gap-x-4">
+              <img src={post.author.imageUrl} alt="" className="h-10 w-10 rounded-full bg-gray-50" />
+              <div className="text-sm leading-6">
+                <p className="font-semibold text-gray-900">
+                  <a href={post.author.href}>
+                    <span className="absolute inset-0" />
+                    {post.author.name}
+                  </a>
+                </p>
+                <p className="text-gray-600">{post.author.role}</p>
+              </div>
+            </div> */}
+          </article>
+        ))}
+      </div>
+
     </div>
   )
+
 }
