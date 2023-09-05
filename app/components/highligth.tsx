7d5e3dd93a5440fa05ee39c6bbe9b9142e7594c8
Lelo <heloisacmk@hotmail.com>
2023-09-05 10:32:16 -0700

    nab bar is black, highlight is bigger

--- app/components/highligth.tsx
+++ app/components/highligth.tsx
@@ -1,8 +1,8 @@
 export default function Highlight() {
     return (
 
-          <div className="w-2/4 flex flex-col justify-center">
-            <img className="py-3 rounded" style={{height: '40vh', objectFit: "cover"}} src="/highlight.jpg" alt="hilight of the day" />
+          <div className="w-full flex flex-col justify-center">
+            <img className="py-3 rounded" style={{height:"40vh", objectFit: "cover"}} src="/highlight.jpg" alt="hilight of the day" />
           </div>
 
     )
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -16,11 +16,12 @@ function classNames(...classes) {
 
 export default function Navbar() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
+    <Disclosure as="nav" className="bg-dark">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
+
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
@@ -33,6 +34,7 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
+
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
                   <img
@@ -42,16 +44,16 @@ export default function Navbar() {
                   />
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
+                  <div className="flex space-x-4 text-kinda-white">
                     {navigation.map((item) => (
                       <a
                         key={item.name}
                         href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium'
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
+                        // className={classNames(
+                        //   item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        //   'rounded-md px-3 py-2 text-sm font-medium'
+                        // )}
+                        // aria-current={item.current ? 'page' : undefined}
                       >
                         {item.name}
                       </a>
@@ -59,7 +61,9 @@ export default function Navbar() {
                   </div>
                 </div>
               </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+              {/* Notification and Profile dropdown */}
+              {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 <button
                   type="button"
                   className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -68,8 +72,6 @@ export default function Navbar() {
                   <span className="sr-only">View notifications</span>
                   <BellIcon className="h-6 w-6" aria-hidden="true" />
                 </button>
-
-                {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
                     <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -125,7 +127,7 @@ export default function Navbar() {
                     </Menu.Items>
                   </Transition>
                 </Menu>
-              </div>
+              </div> */}
             </div>
           </div>
 
