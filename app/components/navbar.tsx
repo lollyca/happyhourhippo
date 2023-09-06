a8e91f60cfda8dcffaf26487cd7fc0e30b9e6f78
Lelo <heloisacmk@hotmail.com>
2023-09-06 09:36:00 -0700

    navbar has dinamic highlighted items - usePathname()

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,8 +1,9 @@
 'use client'
+import { usePathname } from 'next/navigation';
 
 const navigation = [
-  { name: 'Home', href: '/', current: true },
-  { name: 'HappyHours', href: '/happy-hours', current: false },
+  { name: 'Home', href: '/' },
+  { name: 'HappyHours', href: '/happy-hours'},
   // { name: 'Projects', href: '#', current: false },
   // { name: 'Calendar', href: '#', current: false },
 ]
@@ -12,6 +13,8 @@ function classNames(...classes) {
 }
 
 export default function Navbar() {
+  const browserUrlPath = usePathname();
+
   return (
     <div className="h-16 px-2 sm:px-6 lg:px-8 bg-dark flex items-center">
       <div className="mr-4">
@@ -27,7 +30,7 @@ export default function Navbar() {
             key={item.name}
             href={item.href}
           className={classNames(
-            item.current ? 'bg-less-dark' : 'hover:bg-less-dark',
+            browserUrlPath === item.href ? 'bg-less-dark' : 'hover:bg-less-dark',
             'rounded-md px-3 py-2 text-sm font-medium'
           )}
 
